af760d31f848bba7ecf9847cf0e8ac970b98c4dc
Cyrille GEORGEL <cyrille.georgel@gmail.com>
2018-02-12 19:23:39 +0000

    Initial commit

---
+++ resources/views/partContact.blade.php
@@ -0,0 +1,79 @@
+<div class="section landing-section">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+
+            <h2 class="text-center title">Rejoignez-nous.</h2>
+
+
+            <h4 class="text-center description">Tenté de rejoindre notre concept ? Complétez notre formulaire de contact pour obtenir le programme détaillé des stages, les dates des prochaines sessions, un devis pour la formation.<br />
+                Il vous est également possible de créer votre compte Toilettage Process.
+            </h4>
+
+            <form id="contact" action="/contact/" method="post" class="contact-form">
+
+
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group label-floating is-empty">
+                            <label class="control-label">Votre prénom</label>
+                            <input name="firstName" type="text" class="form-control" required>
+                            <span class="material-input"></span></div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group label-floating is-empty">
+                            <label class="control-label">Votre nom</label>
+                            <input name="lastName" type="text" class="form-control" required>
+                            <span class="material-input"></span></div>
+                    </div>
+                </div>
+
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group label-floating is-empty">
+                            <label class="control-label">Votre numéro de téléphone</label>
+                            <input name="phone" type="text" class="form-control" required>
+                            <span class="material-input"></span></div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group label-floating is-empty">
+                            <label class="control-label">Votre adresse EMail</label>
+                            <input type="email" class="form-control" required>
+                            <span class="material-input"></span></div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group label-floating is-empty">
+                            <label class="control-label">Votre demande concerne</label>
+                            <input name="object" type="text" class="form-control" >
+                            <span class="material-input"></span></div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group label-floating is-empty">
+                            <label class="control-label">L'échéance de votre projet</label>
+                            <input name="term" type="text" class="form-control" >
+                            <span class="material-input"></span></div>
+                    </div>
+                </div>
+
+
+
+                <div class="form-group label-floating is-empty">
+                    <label class="control-label">Votre message</label>
+                    <textarea name="message" class="form-control" rows="4"></textarea>
+                    <span class="material-input"></span></div>
+
+                <div class="row">
+                    <div class="col-md-4 col-md-offset-4 text-center">
+                        <button class="btn btn-primary btn-raised">
+                            Envoyer Message
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
---
+++ resources/views/partFooter.blade.php
@@ -0,0 +1,29 @@
+<footer class="footer">
+    <div class="container">
+        <nav class="pull-left">
+            <ul>
+                <li>
+                    <a href="http://toilettagenature.com?utm_source=toilettageprocess.com"
+                    target="_blank">
+                        Toilettage Nature
+                    </a>
+                </li>
+                <li>
+                    <a href="https://checkindog.online?utm_source=toilettageprocess.com"
+                    target="_blank">
+                        Check-in DOG
+                    </a>
+                </li>
+                <li>
+                    <a href="https://forum-toilettage.org?utm_source=toilettageprocess.com"
+                    target="_blank">
+                        Forum du Toilettage
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <div class="copyright pull-right">
+            © 2018, made with <i class="fa fa-heart heart"></i> by BlueRock
+        </div>
+    </div>
+</footer>
