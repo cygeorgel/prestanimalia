ca55f2ffcaba869325db878c4e710136183863f4
Cyrille GEORGEL <cyrille.georgel@gmail.com>
2018-05-23 18:16:08 +0100

    test

--- database/seeds/TrainingSessionsTableSeeder.php
+++ database/seeds/TrainingSessionsTableSeeder.php
@@ -16,83 +16,7 @@ class TrainingSessionsTableSeeder extends Seeder
         DB::table('training_sessions')->insert([
 
 
-//                [
-//                    'training_id' => '1',
-//                    'speciality' => null,
-//                    'start' => '2018-03-26',
-//                    'end' => '2018-03-26',
-//                    'numberOfDays' => 1,
-//                    'priceWithoutTax' => '145',
-//                    'created_at' => NOW(),
-//                    'updated_at' => NOW(),
-//                ],
-//
-//        		[
-//        			'training_id' => '1',
-//                    'speciality' => null,
-//                    'start' => '2018-05-14',
-//                    'end' => '2018-05-14',
-//                    'numberOfDays' => 1,
-//                    'priceWithoutTax' => '145',
-//        			'created_at' => NOW(),
-//        			'updated_at' => NOW(),
-//        		],
-//
-//                [
-//                    'training_id' => '2',
-//                    'speciality' => null,
-//                    'start' => '2018-04-16',
-//                    'end' => '2018-04-16',
-//                    'numberOfDays' => 1,
-//                    'priceWithoutTax' => '145',
-//                    'created_at' => NOW(),
-//                    'updated_at' => NOW(),
-//                ],
-//
-//                [
-//                    'training_id' => '4',
-//                    'speciality' => 'adaptation',
-//                    'start' => '2018-03-26',
-//                    'end' => '2018-03-30',
-//                    'numberOfDays' => 5,
-//                    'priceWithoutTax' => '645',
-//                    'created_at' => NOW(),
-//                    'updated_at' => NOW(),
-//                ],
-//
-//                [
-//                    'training_id' => '4',
-//                    'speciality' => 'adaptation + savoir-faire',
-//                    'start' => '2018-03-26',
-//                    'end' => '2018-04-07',
-//                    'numberOfDays' => 10,
-//                    'priceWithoutTax' => '960',
-//                    'created_at' => NOW(),
-//                    'updated_at' => NOW(),
-//                ],
-//
-//                [
-//                    'training_id' => '4',
-//                    'speciality' => 'adaptation',
-//                    'start' => '2018-05-14',
-//                    'end' => '2018-05-18',
-//                    'numberOfDays' => 5,
-//                    'priceWithoutTax' => '645',
-//                    'created_at' => NOW(),
-//                    'updated_at' => NOW(),
-//                ],
-//
-//                [
-//                    'training_id' => '4',
-//                    'speciality' => 'adaptation + savoir-faire',
-//                    'start' => '2018-05-14',
-//                    'end' => '2018-05-26',
-//                    'numberOfDays' => 10,
-//                    'priceWithoutTax' => '960',
-//                    'created_at' => NOW(),
-//                    'updated_at' => NOW(),
-//                ],
-//
+
                 [
                     'training_id' => '4',
                     'speciality' => 'adaptation',
@@ -114,108 +38,6 @@ class TrainingSessionsTableSeeder extends Seeder
                     'created_at' => NOW(),
                     'updated_at' => NOW(),
                 ],
-
-                [
-                    'training_id' => '5',
-                    'speciality' => 'stage complet (4 semaines)',
-                    'start' => '2018-05-28',
-                    'end' => '2018-06-22',
-                    'numberOfDays' => 20,
-                    'priceWithoutTax' => '1485',
-                    'created_at' => NOW(),
-                    'updated_at' => NOW(),
-                ],
-//
-//                [
-//                    'training_id' => '5',
-//                    'speciality' => 'fourrures épaisses',
-//                    'start' => '2018-07-09',
-//                    'end' => '2018-07-13',
-//                    'numberOfDays' => 5,
-//                    'priceWithoutTax' => '495',
-//                    'created_at' => NOW(),
-//                    'updated_at' => NOW(),
-//                ],
-//
-//                [
-//                    'training_id' => '5',
-//                    'speciality' => 'poil long',
-//                    'start' => '2018-07-16',
-//                    'end' => '2018-07-20',
-//                    'numberOfDays' => 5,
-//                    'priceWithoutTax' => '495',
-//                    'created_at' => NOW(),
-//                    'updated_at' => NOW(),
-//                ],
-//
-//                [
-//                    'training_id' => '5',
-//                    'speciality' => 'poil court',
-//                    'start' => '2018-07-23',
-//                    'end' => '2018-07-27',
-//                    'numberOfDays' => 5,
-//                    'priceWithoutTax' => '495',
-//                    'created_at' => NOW(),
-//                    'updated_at' => NOW(),
-//                ],
-//
-//                [
-//                    'training_id' => '5',
-//                    'speciality' => 'poil dur',
-//                    'start' => '2018-07-30',
-//                    'end' => '2018-08-03',
-//                    'numberOfDays' => 5,
-//                    'priceWithoutTax' => '495',
-//                    'created_at' => NOW(),
-//                    'updated_at' => NOW(),
-//                ],
-//
-//                [
-//                    'training_id' => '5',
-//                    'speciality' => 'stage complet (4 semaines)',
-//                    'start' => '2018-07-09',
-//                    'end' => '2018-08-03',
-//                    'numberOfDays' => 20,
-//                    'priceWithoutTax' => '1485',
-//                    'created_at' => NOW(),
-//                    'updated_at' => NOW(),
-//                ],
-//
-//                [
-//                    'training_id' => '7',
-//                    'speciality' => null,
-//                    'start' => '2018-03-26',
-//                    'end' => '2018-05-05',
-//                    'numberOfDays' => 30,
-//                    'priceWithoutTax' => '1956',
-//                    'created_at' => NOW(),
-//                    'updated_at' => NOW(),
-//                ],
-//
-//                [
-//                    'training_id' => '7',
-//                    'speciality' => null,
-//                    'start' => '2018-05-14',
-//                    'end' => '2018-06-22',
-//                    'numberOfDays' => 30,
-//                    'priceWithoutTax' => '1956',
-//                    'created_at' => NOW(),
-//                    'updated_at' => NOW(),
-//                ],
-//
-//                [
-//                    'training_id' => '7',
-//                    'speciality' => null,
-//                    'start' => '2018-06-25',
-//                    'end' => '2018-08-03',
-//                    'numberOfDays' => 30,
-//                    'priceWithoutTax' => '1956',
-//                    'created_at' => NOW(),
-//                    'updated_at' => NOW(),
-//                ],
-
-                // new dates:
-
 
                 [
                     'training_id' => '4', // natural pro: 1 semaine
@@ -284,11 +106,34 @@ class TrainingSessionsTableSeeder extends Seeder
                     'updated_at' => NOW(),
                 ],
 
+
+            [
+                'training_id' => '5',
+                'speciality' => 'stage complet (4 semaines)',
+                'start' => '2018-05-28',
+                'end' => '2018-06-22',
+                'numberOfDays' => 20,
+                'priceWithoutTax' => '1485',
+                'created_at' => NOW(),
+                'updated_at' => NOW(),
+            ],
+
+            [
+                'training_id' => '5',
+                'speciality' => 'stage complet (4 semaines)',
+                'start' => '2018-07-09',
+                'end' => '2018-08-03',
+                'numberOfDays' => 20,
+                'priceWithoutTax' => '1485',
+                'created_at' => NOW(),
+                'updated_at' => NOW(),
+            ],
+
                 [
                     'training_id' => '5',
                     'speciality' => 'stage complet (4 semaines)',
                     'start' => '2018-09-17',
-                    'end' => '2018-10-13',
+                    'end' => '2018-10-12',
                     'numberOfDays' => 20,
                     'priceWithoutTax' => '1485',
                     'created_at' => NOW(),
@@ -299,7 +144,7 @@ class TrainingSessionsTableSeeder extends Seeder
                     'training_id' => '5',
                     'speciality' => 'stage complet (4 semaines)',
                     'start' => '2018-10-29',
-                    'end' => '2018-11-24',
+                    'end' => '2018-11-23',
                     'numberOfDays' => 20,
                     'priceWithoutTax' => '1485',
                     'created_at' => NOW(),
@@ -310,18 +155,30 @@ class TrainingSessionsTableSeeder extends Seeder
                     'training_id' => '5',
                     'speciality' => 'stage complet (4 semaines)',
                     'start' => '2018-12-10',
-                    'end' => '2019-01-19',
+                    'end' => '2019-01-18',
                     'numberOfDays' => 20,
                     'priceWithoutTax' => '1485',
                     'created_at' => NOW(),
                     'updated_at' => NOW(),
                 ],
 
+
+            [
+                'training_id' => '7', // 6 semaines
+                'speciality' => null,
+                'start' => '2018-06-25',
+                'end' => '2018-08-03',
+                'numberOfDays' => 30,
+                'priceWithoutTax' => '1956',
+                'created_at' => NOW(),
+                'updated_at' => NOW(),
+            ],
+
                 [
                     'training_id' => '7', // 6 semaines
                     'speciality' => null,
                     'start' => '2018-09-03',
-                    'end' => '2018-10-13',
+                    'end' => '2018-10-12',
                     'numberOfDays' => 30,
                     'priceWithoutTax' => '1956',
                     'created_at' => NOW(),
@@ -332,7 +189,7 @@ class TrainingSessionsTableSeeder extends Seeder
                     'training_id' => '7', // 6 semaines
                     'speciality' => null,
                     'start' => '2018-10-15',
-                    'end' => '2018-11-24',
+                    'end' => '2018-11-23',
                     'numberOfDays' => 30,
                     'priceWithoutTax' => '1956',
                     'created_at' => NOW(),
@@ -343,7 +200,7 @@ class TrainingSessionsTableSeeder extends Seeder
                     'training_id' => '7', // 6 semaines
                     'speciality' => null,
                     'start' => '2018-11-26',
-                    'end' => '2019-01-19',
+                    'end' => '2019-01-18',
                     'numberOfDays' => 30,
                     'priceWithoutTax' => '1956',
                     'created_at' => NOW(),
