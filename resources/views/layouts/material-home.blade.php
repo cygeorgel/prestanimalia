d7f5467ed97aee59c51bf6ad218c52000d93d74c
Cyrille GEORGEL <cyrille.georgel@gmail.com>
2019-02-19 11:30:22 +0000

    header

--- resources/views/layouts/material-home.blade.php
+++ resources/views/layouts/material-home.blade.php
@@ -6,18 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="viewport" content="width=device-width" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'TOILETTAGE PROCESS') }}</title>
+    <title>{{ config('app.name', 'Prestanimalia') }}</title>
     <meta name="author" content="BlueRock < https://bluerock.ie >" />
-    <meta property="og:url" content="https://toilettageprocess.com/" />
-    <meta property="og:title" content="Toilettage Process" />
+    <meta property="og:url" content="https://prestanimalia.org/" />
+    <meta property="og:title" content="Prestanimalia Process" />
     <meta property="og:locale" content="fr_FR" />
-    <meta property="og:description" content="Un souffle nouveau sur la profession toiletteur" />
+    <meta property="og:description" content="Prestanimalia, Chambre nationale des prestataires animaliers, représente les toiletteurs, les éducateurs canins et les autres professions de service auprès des animaux de compagnie. Prestanimalia est le créateur et l'organisateur du Forum du Toilettage et à l'initiative de la création de la classe Toilettage Animalier au sein du Concours un des Meilleurs Ouvriers de France." />
     <meta property="og:type" content="website" />
-    <meta property="og:image" content="https://toilettageprocess.com/storage/opengraph.jpg" />
-    <meta property="fb:app_id" content="1076801095670842" />
+    <meta property="og:image" content="https://forum-toilettage.org/assets/img/forumToilettageOpengraph.jpg" />
+    <meta property="fb:app_id" content="188403184953931" />
 
     <!-- Canonical SEO -->
-    <link rel="canonical" href="https://toilettageprocess.com/">
+    <link rel="canonical" href="https://prestanimalia.org/">
 
     <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/apple-icon.png') }}" />
     <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}" />
@@ -37,7 +37,7 @@
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
 
-      gtag('config', 'UA-38512129-10');
+      gtag('config', 'UA-38510644-16);
     </script>
 
     @stack('styles')
